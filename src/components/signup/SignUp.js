0119cd89bd64229676cd3f111f1225184825b50e
SarahABKH <107299480+SarahABKH@users.noreply.github.com>
2023-12-11 07:13:13 +0530

    second

---
+++ src/components/signup/SignUp.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+
+const SignUp = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your sign-up logic here
+    console.log('Form submitted:', formData);
+    // You can call an API to handle the actual sign-up process
+  };
+
+  return (
+    <div>
+      <center><h2>Sign Up</h2></center>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">Sign Up</button>
+      </form>
+      <br></br>
+    </div>
+  );
+};
+
+export default SignUp;
+
